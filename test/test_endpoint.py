28f0d1a9586d43b391322dda829f51a7135c12b1
santibianco <santiago.bianco.sb@gmail.com>
2020-02-07 20:22:12 -0300

    updated test files structure

--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -1,10 +1,15 @@
 import pytest
 import requests
 
+ENDPOINT = "http://localhost:8000/run
+
 class TestEndpoint(object):
 
-    @pytest.mark.parametrize("endpoint", ["http://localhost:8000/run"])
+    @pytest.mark.parametrize("endpoint", [ENDPOINT])
+    def test_endpointIsUp(self, endpoint):
+        assert requests.get(endpoint).status_code == 200
+
+
+    @pytest.mark.parametrize("endpoint", [ENDPOINT])
     def test_retreiveEndpointData(self, endpoint):
-        response = requests.get(endpoint)
-        assert response.status_code == 200
-        assert response.json()['current_url'] == "/run"
+        assert requests.get(endpoint).json()['current_url'] == "/run"

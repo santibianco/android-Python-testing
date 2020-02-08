2bb4593b48a17730c1660928a1045c11bb7e80f6
santibianco <santiago.bianco.sb@gmail.com>
2020-02-08 19:53:45 -0300

    added final tests

--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -1,15 +1,25 @@
 import pytest
 import requests
 
-ENDPOINT = "http://localhost:8000/run
+BASE_URL = "http://localhost:8000"
+ENDPOINT = BASE_URL + "/run"
+TEST_APP = "speedtest.apk"
 
 class TestEndpoint(object):
 
-    @pytest.mark.parametrize("endpoint", [ENDPOINT])
+    @pytest.mark.parametrize("endpoint", [BASE_URL])
     def test_endpointIsUp(self, endpoint):
         assert requests.get(endpoint).status_code == 200
 
 
-    @pytest.mark.parametrize("endpoint", [ENDPOINT])
+    @pytest.mark.parametrize("endpoint", [BASE_URL])
     def test_retreiveEndpointData(self, endpoint):
-        assert requests.get(endpoint).json()['current_url'] == "/run"
+        assert len(requests.get(endpoint).json()['available_apks']) > 0
+
+
+    @pytest.mark.parametrize("endpoint, app", [(ENDPOINT, TEST_APP)])
+    def test_endpointRun(self, endpoint, app):
+        response = requests.get(endpoint, params={"name" : app}).json()
+        assert response['installation_status'] == "Success"
+        assert response['running_status'] == "Success"
+        assert "Error" not in response["screenshot_status"]
